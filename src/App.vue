93356b7d20db03e3f60c409979f59245e559e2b9
Oralita <oat431@gmail.com>
2021-07-08 17:28:14 +0700

    [🍷 refactor] : the code

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <table>
+    <thead>
+      <th>name</th>   
+      <th>age</th>   
+    </thead>  
+    <tbody >
+      <tr v-for="item in friends" :key=item.id>
+        <Profile :name=item.name :age=item.age /> 
+      </tr>
+    </tbody>
+  </table> 
+</template>
+
+<script>
+import Profile from "./components/Profile.vue";
+
+export default {
+  name: "App",
+  components: {
+    Profile,
+  },
+  data() {
+    return {
+      friends: [
+        {id:1, name: "Kong", age: 21 },
+        {id:2, name: "Oat", age: 20 },
+        {id:3, name: "Pun", age: 22 },
+        {id:4, name: "Fax", age: 21 },
+        {id:5, name: "DJ", age: 20 },
+        {id:6,name: "Teem", age: 20 },
+        {id:7, name: "Tar", age: 20 },
+        {id:8, name: "Tle", age: 21 },
+        {id:9, name: "Pluk", age: 20 },
+        {id:10, name: "Michael", age: 20 },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
